6348a8ad6de5586f7f3851b9a4b64b2558ba84d7
Martin Weber <martin.weber@de.clara.net>
2017-11-02 09:11:49 +0100

    normalize to lowercase

--- collector/collector.go
+++ collector/collector.go
@@ -12,6 +12,7 @@ func normalizeFQN(fqn string) string {
 	var _fqn string = fqn
 	_fqn = strings.Replace(_fqn, ".", "_", -1)
 	_fqn = strings.Replace(_fqn, "-", "_", -1)
+	_fqn = strings.ToLower(_fqn)
 
 	return _fqn
 }
